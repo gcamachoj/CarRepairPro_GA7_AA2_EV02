9e875113708d4fd2c335c31bf8fb6d99d9e1a62c
GUILLERMO CAMACHO <guiller.camacho.jara@gmail.com>
2024-06-04 09:19:49 -0500

    comentarios en vehiculos.rotues

--- vehiculos/routes.py
+++ vehiculos/routes.py
@@ -1,14 +1,18 @@
 from flask import Blueprint, render_template, request, redirect, flash, url_for
-from modelos import Vehiculo, Marca, Tipos_Carroceria, Ciudad
-from extensions import db
-
-#from modelos import Vehiculo
+from modelos import Vehiculo, Marca, Tipos_Carroceria, Ciudad  # Importacion de modelos de datos del orm
+from extensions import db  # importar servicio db para instanciar sqlalchemy y manipular los modelos de datos
 
 # Define un blueprint para las rutas de los templates de vehículos
 vehiculos_bp = Blueprint('vehiculos_bp', __name__, template_folder='../templates/vehiculos')
 
 
-@vehiculos_bp.route('/vehiculos') # Listar vehículos
+#========================================================================================================
+# ***** ENDPOINTS VEHICULOS (RUTAS) ********************************************************************** 
+#=========================================================================================================
+
+# ----1. Listar vehículos ---------------------------------------------------------------------------------
+
+@vehiculos_bp.route('/vehiculos') 
 def vehiculos():
     template_folder_path = vehiculos_bp.template_folder
     vehiculos = Vehiculo.query.all() 
@@ -19,14 +23,19 @@ def vehiculos():
     # Renderizar el template con la lista vehiculos
     return render_template('vehiculos.html', vehiculos = vehiculos) 
 
-@vehiculos_bp.route('/vehiculos/crear') # Abrir formulario de crear vehiculo
+
+# ---- 2.1 Abrir formulario de crear vehiculo .............................................................
+
+@vehiculos_bp.route('/vehiculos/crear') 
 def crear_vehiculo():
     ciudades = Ciudad.query.all()
     marcas = Marca.query.all()
     carrocerias = Tipos_Carroceria.query.all()
     return render_template('crear_vehiculo.html', ciudades=ciudades, marcas=marcas, carrocerias = carrocerias)
 
-@vehiculos_bp.route('/vehiculos/guardar_vehiculo', methods=["POST"])
+# ---- 2.2 Guardar datos del vehiculo # --------------------------------------------------------------------
+
+@vehiculos_bp.route('/vehiculos/guardar_vehiculo', methods=["POST"])  #
 def guardar_vehiculo():
     vehiculo = Vehiculo(
                       Placa = request.form['InputPlaca'],
@@ -43,6 +52,7 @@ def guardar_vehiculo():
     return redirect(url_for('vehiculos_bp.vehiculos'))
 
 
+# 3.---- Filtrar vehiculo -----------------------------------------------------------------------------------
 @vehiculos_bp.route('/vehiculos/<int:vehiculo_id>')
 def detalleVehiculos(vehiculo_id):
     vehiculo= Vehiculo.query.get_or_404(vehiculo_id)
@@ -50,7 +60,7 @@ def detalleVehiculos(vehiculo_id):
     return render_template('vehiculos/detalle_vehiculos.html', vehiculo=vehiculo) 
 
 
-
+# 4 ---- actualizar Vehículo ---------------------------------------------------------------------------------
 @vehiculos_bp.route('/actualizar-vehiculo/<int:vehiculo_id>', methods=['GET','POST'])
 def actualizar_vehiculo(vehiculo_id):
     vehiculo = Vehiculo.query.filter_by(IdVehiculo = int(vehiculo_id)).first()
@@ -76,11 +86,12 @@ def actualizar_vehiculo(vehiculo_id):
             flash('¡Vehículo actualizado exitosamente!', 'success')
             return redirect(url_for('vehiculos_bp.vehiculos'))
         else:
-            return render_template('vehiculos/actualizar_vehiculo.html', vehiculo=vehiculo, marcas=marcas, ciudades = ciudades, tipos_carroceria = tipos_carroceria)
+            return render_template('vehiculos/actualizar_vehiculo.html', 
+            vehiculo=vehiculo, marcas=marcas, ciudades = ciudades, tipos_carroceria = tipos_carroceria)
     else:
         return "vehículo no encontrado", 404
     
-
+# ---- 5. Eliminar Vehículo -------------------------------------------------------------------------------------------
 
 @vehiculos_bp.route('/eliminar-vehiculo/<int:vehiculo_id>')
 def eliminarVehiculo(vehiculo_id):
