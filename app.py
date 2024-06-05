48a12c984b7acc5465f7440be5925f595ec957e5
GUILLERMO CAMACHO <guiller.camacho.jara@gmail.com>
2024-06-05 12:19:23 -0500

    Se facturiza para modularizar los servicios del cliente

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from extensions import db  # Importa db desde extensions.py
 # Registra las rutas del componente vehiculos en el archivo app.py
 from modulos.vehiculos.routes import vehiculos_bp 
 from modulos.ordenes.routes import ordenes_bp 
+from modulos.clientes.routes import clientes_bp
 from modelos import Cliente, TipoCliente, Ciudad # Importamos el modelo ciudad del archivo modelos
 
 # Instanciamos la aplicacion
@@ -22,6 +23,7 @@ app.config['SECRET_KEY'] = 'SENA'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost/taller'  
 app.register_blueprint(vehiculos_bp)
 app.register_blueprint(ordenes_bp)
+app.register_blueprint(clientes_bp)
 # c# Inicializa la extensión SQLAlchemy
 db.init_app(app)
 
@@ -39,80 +41,7 @@ def login():
 @app.route('/index')
 def index():
     return render_template('index.html')
-    
-# === ENDPOINTS CLIENTES =============================================================
-#***********************************************************************************
-@app.route('/clientes')
-def clientes():
-    clientes = Cliente.query.all() # Consulta todos los clientes en la base de datos
-    return render_template('clientes/clientes.html', clientes=clientes)
-
-@app.route('/crear_cliente')
-def crear_cliente():
-    ciudades = Ciudad.query.all()
-    tiposCliente = TipoCliente.query.all()
-    return render_template('clientes/crear_cliente.html', ciudades=ciudades, tiposCliente=tiposCliente)
-
-
-
-@app.route('/clientes/guardar_cliente', methods=["POST"])
-def guardar_cliente():
-    cliente = Cliente(
-                      IdTipoCliente=request.form['InputIdTipoCliente'], 
-                      CC_NIT = request.form['InputCC_Nit'],
-                      Nombres = request.form['InputNombres'], 
-                      IdCiudad= request.form['InputIdCiudad'], 
-                      Direccion=request.form['InputDireccion'],
-                      email=request.form['InputEmail'],
-                      telefono= request.form['InputTelefono']                
-                      )
-    db.session.add(cliente)
-    db.session.commit()
-    flash('¡Cliente guardado exitosamente!', 'success')
-    return redirect(url_for('clientes'))
-
-
-@app.route('/clientes/<int:cliente_id>')
-def detalleClientes(cliente_id):
-    cliente= Cliente.query.get_or_404(cliente_id)
-    tiposCliente = TipoCliente.query.all()
-    ciudades = Ciudad.query.all()
-    return render_template('clientes/detalle_clientes.html', cliente=cliente, tiposCliente = tiposCliente, ciudades = ciudades) 
  
-
-@app.route('/eliminar-cliente/<int:cliente_id>')
-def eliminarCliente(cliente_id):
-    Cliente.query.filter_by(IdCliente = int(cliente_id)).delete()
-    db.session.commit()   
-    flash('¡Cliente eliminado exitosamente!', 'success')
-    return redirect(url_for('clientes')) 
-#------------------------------------------------------
-
-@app.route('/actualizar-cliente/<int:cliente_id>', methods=['GET','POST'])
-def actualizar_cliente(cliente_id):
-    cliente = Cliente.query.filter_by(IdCliente = int(cliente_id)).first()
-    tiposCliente = TipoCliente.query.all()
-    ciudades = Ciudad.query.all()
-    
-    if cliente:
-        if request.method == 'POST':
-            # Actualizar datos del cliente
-            cliente.Nombres = request.form['InputNombres']
-            cliente.IdTipoCliente=request.form['InputIdTipoCliente']
-            cliente.CC_NIT = request.form['InputCC_Nit']
-            cliente.IdCiudad= request.form['InputIdCiudad']
-            cliente.Direccion=request.form['InputDireccion']
-            cliente.email=request.form['InputEmail']
-            cliente.telefono= request.form['InputTelefono'] 
-            db.session.commit()
-            flash('¡Cliente actualizado exitosamente!', 'success')
-            return redirect(url_for('clientes'))
-        else:
-            return render_template('clientes/actualizar_cliente.html', cliente=cliente, tiposCliente = tiposCliente, ciudades = ciudades)
-    else:
-        return "Cliente no encontrado", 404
-
-
 #Ejecucion
     
 if __name__ == "__main__":
---
+++ modulos/clientes/routes.py
@@ -0,0 +1,90 @@
+# -- Importación de librerías
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from extensions import db  # importar servicio db para instanciar sqlalchemy y manipular los modelos de datos
+from modelos import Cliente, Ciudad, TipoCliente
+
+clientes_bp = Blueprint('clientes_bp', __name__, template_folder = '../../templates/clientes')
+
+
+# === ENDPOINTS CLIENTES =============================================================
+#***********************************************************************************
+
+#---LISTAR CLIENTES ---------------------------------------------------------------
+
+@clientes_bp.route('/clientes')
+def clientes():
+    clientes = Cliente.query.all() # Consulta todos los clientes en la base de datos
+    return render_template('clientes/clientes.html', clientes=clientes)
+
+
+# -- FORMULARIO CREAR CLIENTE ------------------------------------------------------
+
+@clientes_bp.route('/crear_cliente')
+def crear_cliente():
+    ciudades = Ciudad.query.all()
+    tiposCliente = TipoCliente.query.all()
+    return render_template('clientes/crear_cliente.html', ciudades=ciudades, tiposCliente=tiposCliente)
+
+
+
+# -- FORMULARIO GUARDAR CLIENTE  ------------------------------------------------------
+
+@clientes_bp.route('/clientes/guardar_cliente', methods=["POST"])
+def guardar_cliente():
+    cliente = Cliente(
+                      IdTipoCliente=request.form['InputIdTipoCliente'], 
+                      CC_NIT = request.form['InputCC_Nit'],
+                      Nombres = request.form['InputNombres'], 
+                      IdCiudad= request.form['InputIdCiudad'], 
+                      Direccion=request.form['InputDireccion'],
+                      email=request.form['InputEmail'],
+                      telefono= request.form['InputTelefono']                
+                      )
+    db.session.add(cliente)
+    db.session.commit()
+    flash('¡Cliente guardado exitosamente!', 'success')
+    return redirect(url_for('clientes_bp.clientes'))
+
+# ---DETALLE DE CLIENTE  ----------------------------------------------------------
+@clientes_bp.route('/clientes/<int:cliente_id>')
+def detalleClientes(cliente_id):
+    cliente= Cliente.query.get_or_404(cliente_id)
+    tiposCliente = TipoCliente.query.all()
+    ciudades = Ciudad.query.all()
+    return render_template('clientes/detalle_clientes.html', cliente=cliente, tiposCliente = tiposCliente, ciudades = ciudades)
+
+#--- MODIFICAR DATOS CLIENTE ------------------------------------------------------
+
+@clientes_bp.route('/actualizar-cliente/<int:cliente_id>', methods=['GET','POST'])
+def actualizar_cliente(cliente_id):
+    cliente = Cliente.query.filter_by(IdCliente = int(cliente_id)).first()
+    tiposCliente = TipoCliente.query.all()
+    ciudades = Ciudad.query.all()
+    
+    if cliente:
+        if request.method == 'POST':
+            # Actualizar datos del cliente
+            cliente.Nombres = request.form['InputNombres']
+            cliente.IdTipoCliente=request.form['InputIdTipoCliente']
+            cliente.CC_NIT = request.form['InputCC_Nit']
+            cliente.IdCiudad= request.form['InputIdCiudad']
+            cliente.Direccion=request.form['InputDireccion']
+            cliente.email=request.form['InputEmail']
+            cliente.telefono= request.form['InputTelefono'] 
+            db.session.commit()
+            flash('¡Cliente actualizado exitosamente!', 'success')
+            return redirect(url_for('clientes_bp.clientes'))
+        else:
+            return render_template('clientes/actualizar_cliente.html', cliente=cliente, tiposCliente = tiposCliente, ciudades = ciudades)
+    else:
+        return "Cliente no encontrado", 404
+
+
+# ---ELIMINAR CLIENTE  ----------------------------------------------------------
+
+@clientes_bp.route('/eliminar-cliente/<int:cliente_id>')
+def eliminarCliente(cliente_id):
+    Cliente.query.filter_by(IdCliente = int(cliente_id)).delete()
+    db.session.commit()   
+    flash('¡Cliente eliminado exitosamente!', 'success')
+    return redirect(url_for('clientes_bp.clientes')) 
