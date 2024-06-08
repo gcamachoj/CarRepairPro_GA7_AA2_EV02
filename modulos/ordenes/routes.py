9b45f24cb117490a7ccf151740025c3ae496c101
GUILLERMO CAMACHO <guiller.camacho.jara@gmail.com>
2024-06-08 08:04:08 -0500

    Se corrigen novedades de modulo actualizar_vehiculo.html por campos que no se guardaban correctamente. Se finaliza funcionalidad actualizar_orden

--- modulos/ordenes/routes.py
+++ modulos/ordenes/routes.py
@@ -73,10 +73,57 @@ def guardar_orden():
         print(e)  # Puedes imprimir el error para depurar si algo sale mal
         return redirect(url_for('ordenes_bp.ordenes'))
     
-# 3.---- Filtrar vehiculo -----------------------------------------------------------------------------------
+# 3.---- Filtrar Orden  (Detalles)-------------------------------------------------------------------------------
 
 @ordenes_bp.route('/ordenes/<int:orden_id>')
 def detalleOrden(orden_id):
     orden= Orden.query.get_or_404(orden_id)
 
     return render_template('ordenes/detalle_orden.html', orden=orden) 
+# 4 ---- actualizar Orden ---------------------------------------------------------------------------------
+@ordenes_bp.route('/actualizar-orden/<int:orden_id>', methods=['GET','POST'])
+def actualizar_orden(orden_id):
+    orden = Orden.query.filter_by(IdOrden = int(orden_id)).first()
+    clientes = Cliente.query.all()
+    vehiculos = Vehiculo.query.all()
+    empleados = Empleado.query.all()
+    estados_os = Estado_os.query.all()
+
+
+    if orden:
+        if request.method == 'POST':
+            # Imprimir los datos recibidos
+            print("Datos recibidos:", request.form)
+
+            # Actualizar datos 
+   # Actualizar datos
+            orden.IdCliente = request.form['InputIdCliente']
+            orden.IdVehiculo = request.form['InputIdVehiculo']
+            orden.IdMecanico = request.form['InputIdMecanico']
+            orden.IdEstadoOS = request.form['InputIdEstadoOS']
+            orden.KM_Entrada = request.form['InputKM_Entrada']
+            
+            # Manejar el valor de KM_Salida, podría estar vacío
+            km_salida = request.form['InputKM_Salida']
+            if km_salida =='None':
+                km_salida = None
+            else:
+                km_salida = int(km_salida) if km_salida.strip() else None
+            orden.KM_Salida = km_salida
+            # Manejar el valor de FechaFinServicio, podría estar vacío
+            fecha_fin_servicio = request.form['InputFechaFinServicio']
+            orden.FechaFinServicio = fecha_fin_servicio.strip() if fecha_fin_servicio.strip() else None
+            
+            orden.FechaIngreso = request.form['InputFechaIngreso']
+            orden.Observaciones = request.form['InputObservaciones']
+          
+            
+            db.session.commit()
+            flash('Orden actualizada exitosamente!', 'success')
+            return redirect(url_for('ordenes_bp.ordenes'))
+        else:
+            return render_template('ordenes/actualizar_orden.html', 
+            orden=orden, clientes=clientes, vehiculos=vehiculos, empleados=empleados, estados_os=estados_os)
+    else:
+        return "Orden no encontrado", 404
+    
